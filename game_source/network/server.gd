17b161c95751ef58810bad1b5359f9004ca785a7
Alex Hoffman <alxhoff@gmail.com>
2019-09-27 11:16:28 +0200

    added mac addresse for the beacons

--- game_source/network/server.gd
+++ game_source/network/server.gd
@@ -1,11 +1,11 @@
 extends Node
 
-const test_mac_1 = [0x00,0x00,0x00,0x00,0x00,0x01]
-const test_mac_2 = [0x00,0x00,0x00,0x00,0x00,0x02]
-const test_mac_3 = [0x00,0x00,0x00,0x00,0x00,0x03]
-const test_mac_4 = [0x00,0x00,0x00,0x00,0x00,0x04]
-const test_mac_5 = [0x00,0x00,0x00,0x00,0x00,0x05]
-const test_mac_6 = [0x00,0x00,0x00,0x00,0x00,0x06]
+const test_mac_1 = [0xA0,0xE6,0xF8,0xB6,0x10,0x00]
+const test_mac_2 = [0xA0,0xE6,0xF8,0xB6,0x5D,0x05]
+const test_mac_3 = [0xA0,0xE6,0xF8,0xB6,0x5B,0x83]
+const test_mac_4 = [0xB8,0x27,0xEB,0x9A,0x9F,0x91]
+const test_mac_5 = [0xB8,0x27,0xEB,0x5E,0xCB,0xFD]
+const test_mac_6 = [0xB8,0x27,0xEB,0x7F,0x96,0xF9]
 
 const test_macs = [test_mac_1, test_mac_2, test_mac_3, test_mac_4, test_mac_5, test_mac_6]
 
@@ -27,7 +27,7 @@ func _process(delta):
 		var data = socketUDP.get_packet()
 		var IP_CLIENT = socketUDP.get_packet_ip()
 		var PORT_CLIENT = socketUDP.get_packet_port()
-#		print("Data recv on port %d from ip %s" % [PORT_CLIENT, IP_CLIENT])
+		print("Data recv on port %d from ip %s" % [PORT_CLIENT, IP_CLIENT])
 		if data:
 			process_packet(data)
 		
@@ -36,7 +36,7 @@ func process_packet(packet): #Rssi packet is 2 bytes, first byte is device id, s
 	if header == RSSI_PACKET_HEADER:
 		var bt_device = packet.subarray(1,6)
 		var bt_rssi = packet.subarray(7,7)[0]
-#		print("BT DEV: %x:%x:%x:%x:%x:%x RSSI: %d" % [bt_device[0],bt_device[1],bt_device[2],bt_device[3],bt_device[4],bt_device[5], bt_rssi])
+		print("BT DEV: %x:%x:%x:%x:%x:%x RSSI: %d" % [bt_device[0],bt_device[1],bt_device[2],bt_device[3],bt_device[4],bt_device[5], bt_rssi])
 		bt_rssi = float(bt_rssi) / 255 * 100.0
 		var device_id = match_device(bt_device)
 		emit_signal("new_bt_rssi", device_id, round(bt_rssi))
